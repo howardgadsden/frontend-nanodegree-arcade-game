c7af023d1f4e188e2b73f15a2c974aac2e31f6ac
howardgadsden <barbarachasehoward@gmail.com>
2016-02-17 04:25:25 -0500

    Clean up code, add comments, update README file.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-Students should use this [rubric](https://www.udacity.com/course/viewer/#!/c-nd001/l-2696458597/m-2687128535) for self-checking their submission.
+The game is started by opening index.html in a browser.
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+The objective of the game is to move the player to the water without colliding with an enemy. Enemies are the bugs crossing the game board from left to right. The left, right, up, and down arrows on the keyboard are used to move the player on the board. If the player collides with an enemy, the player is moved back to the start position. The cumulative number of collisions is displayed below the game board. Once the player reaches the water, a message is displayed. The message includes the number of times the player has won the game and instructs the user to press "Enter" to start over. When "Enter" is pressed, the player is moved back to the start position.
--- js/app.js
+++ js/app.js
@@ -1,67 +1,64 @@
-// This is the enemy constuctor. It initializes the x and y coordinates of the enemy as well as teh initial square that teh enemy occupies
-// The squares are used for collision detection
-// The coordinates are for the upper left hand corner of hte image. Images are 101 x 171
-// the initial row and speed for each enemy is randomly selected
-// Finally, it loads the enemy image into memory
+/* This is the enemy class constuctor.
+It loads the image for the enemy into memory using the load function provided in resources.js.
+It initializes the x and y coordinates of the enemy as well as the initial square that the enemy occupies
+The squares are used for collision detection.
+The coordinates are for the upper left hand corner of the image. Images are 101 x 171.
+The initial row and speed for each enemy is randomly selected. */
 var Enemy = function() {
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-
     this.sprite = 'images/enemy-bug.png';
     Resources.load(this.sprite);
-    this.setEnemyParameters();
+    this.setParameters();
     this.square = getSquare(this.x, this.y);
 };
 
 
-// Update the enemy's position.
-// Parameter: dt, a time delta between ticks
+/* Method to update the enemy's position.
+Parameter: dt, a time delta between ticks
+The speed is multiplied by the dt parameter
+which will ensure the game runs at the same speed for
+all computers. */
 Enemy.prototype.update = function(dt) {
-    // The speed is multiplied by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-
     this.x += this.speed * dt;
     if (this.x > 480)
-        this.setEnemyParameters();
+        this.setParameters();
     this.square = getSquare(this.x, this.y);
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
     if (this.square == player.square) {
         player.collisions += 1;
         player.reset();
-        ctx.font="16px Verdana";
-        ctx.strokeStyle='red';
+        ctx.font = '16px Verdana';
+        ctx.strokeStyle = 'red';
         if (player.collisions == 1)
-            ctx.strokeText("Collision count = ", 0, 606);
+            ctx.strokeText('Collision count = ', 0, 606);
 
-       ctx.fillStyle = 'white';
-       ctx.fillRect(160,550,200,100);
-       ctx.strokeText(player.collisions,160,606);
+        ctx.fillStyle = 'white';
+        ctx.fillRect(160, 550, 200, 100);
+        ctx.strokeText(player.collisions, 160, 606);
     }
 };
 
-Enemy.prototype.setEnemyParameters = function() {
+/* Method to initialize the x, y, and speed attributes of the enemy object.
+A random integer ranging from 0 to 2 is used to randomize the inital position and speed of each enemy object. */
+Enemy.prototype.setParameters = function() {
     var num = Math.floor(Math.random() * 3);
     this.x = 0;
     this.y = 83 * num + 68;
     this.speed = 75 * (num + 1);
-}
+};
 
 
-// Draw the enemy on the screen, required method for game
+// Function to draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
 
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    var allowedKeys = {
-        37: 'left',
-    };
+
 };
 
-// This is the player class constructor
-// It initalizes the x and y coordinates for the player and also initializes the square that the player occupies
-// The square is used for collision detection and also to determine when the game is over
-// Finally, it loads the image for the player into memory
+/* This is the player class constructor.
+It loads the image for the player into memory using the load function provided in resources.js.
+It initalizes the x and y coordinates for the player and also initializes the square that the player occupies.
+The square is used for collision detection and also to determine when the game is over. */
 var Player = function() {
     this.sprite = 'images/char-pink-girl.png';
     // Inital coordinates place the player in the center of the bottom row.
@@ -72,65 +69,68 @@ var Player = function() {
     this.wins = 0;
     this.winFlag = 0;
     this.collisions = 0;
-    this.collisionFlag = 0;
     Resources.load(this.sprite);
-   }
+};
 
-// Hnadle game wins here
+/* Method to update the player object - called on each loop of the game engine.
+It detects when the player has reached the water and implements the functionality for winning the game..
+It increments the number of wins and writes a message to the screen. */
 Player.prototype.update = function() {
-    if (this.square < 5 && this.winFlag < 1)  {// Got past all enemies and won the game
-        var gradient = ctx.createLinearGradient(0,0,505,0);
+    // the winFlag is used to ensure that the code to handle the win is only executed once per win.
+    if (this.square < 5 && this.winFlag < 1) { // Got past all enemies and won the game
+        var gradient = ctx.createLinearGradient(0, 0, 505, 0);
 
         this.wins += 1;
         this.winFlag = 1;
-        ctx.font="18px Verdana";
-        gradient.addColorStop("0","magenta");
-        gradient.addColorStop("0.5","blue");
-        gradient.addColorStop("1.0","red");
+        ctx.font = '18px Verdana';
+        gradient.addColorStop('0', 'magenta');
+        gradient.addColorStop('0.5', 'blue');
+        gradient.addColorStop('1.0', 'red');
         // Fill with gradient
-        ctx.strokeStyle=gradient;
+        ctx.strokeStyle = gradient;
         if (this.wins == 1) {
-            ctx.strokeText("Congratulations on win # ", 0, 40);
-            ctx.strokeText("Press Enter to start again.",250, 40);
-        };
+            ctx.strokeText('Congratulations on win # ', 0, 40);
+            ctx.strokeText('Press Enter to start again.', 250, 40);
+        }
         ctx.fillStyle = 'white';
-        ctx.fillRect(230, 20,20, 25);
+        ctx.fillRect(230, 20, 20, 25);
         ctx.strokeText(this.wins, 230, 40);
-  }
+    }
 };
 
-// Reset teh player to initial conditions
-// Used when a collision is detected and also when the player wins the game
+/* Method to reset the player to initial conditions -
+used when a collision is detected and also when the player wins the game */
 Player.prototype.reset = function() {
-   this.x = 200;
-   this.y = 405;
-   this.square = getSquare(this.x, this.y);
-   this.winFlag = 0;
-   this.collisionFlag = 0;
+    this.x = 200;
+    this.y = 405;
+    this.square = getSquare(this.x, this.y);
+    this.winFlag = 0;
+};
+
+/* Function to determine which square an object occupies.
+Squares are numbered 0 through 29 where the top row is numbered 0 through 4, second row 5 through 9, etc.
+Have to compensate for the size of the images when specifying the coordinated of the object.
+All images are 101 x 117 and are drawn from upper left corner */
+function getSquare(x, y) {
+    var col = Math.floor((x + 55) / 101);
+    var row = Math.floor((y + 70) / 83);
+
+    return row * 5 + col;
 }
 
-//Squares are numbered 0 through 29 where the top row is numbered 0 through 4, second row 5 through 9, etc.
-// Have to compensate for hte size of hte images
-// All images are 101 x 117 and are drawn from upper left corner
-
-function getSquare(x,y) {
-        var col = Math.floor((x+55)/101);
-        var row = Math.floor((y+70)/83);
-
-
-        return row * 5 + col;
-}
-
-    // Draw the player on the board
+// Method to draw the player on the board
 Player.prototype.render = function(dt) {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Handle up, down, right, and left arrow input to move player around board
+/* Method to handle keyboard input.
+Handle up, down, right, and left arrow input to move the player around the board.
+Also handles the  'Enter' key - used to restart the game after a win. */
 Player.prototype.handleInput = function(keyCode) {
-        y = this.y,
-        x = this.x;
+    var x = this.x;
+    var y = this.y;
 
+    // In each case, the player is prevented from moving off teh board.
     if (keyCode == 'left') {
         this.x = this.x - 101;
         if (this.x < -50)
@@ -149,35 +149,35 @@ Player.prototype.handleInput = function(keyCode) {
     if (keyCode == 'down') {
         this.y = player.y + 83;
         if (this.y > 480)
-            this.y= y;
+            this.y = y;
     }
     if (keyCode == 'enter') this.reset();
 
     this.square = getSquare(this.x, this.y);
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+/* All  ojects are instantiated here.
+Enemy objects are placed in an array called allEnemies.
+The player object is placed in a variable called player. */
 
 var allEnemies = [];
-var player = new Player;
+var player = new Player();
+var enemy;
 
 for (var i = 0; i < 3; i++) {
-    enemy = new Enemy;
+    enemy = new Enemy();
     enemy.x = 0;
     enemy.y = i * 83 + 68;
     enemy.square = getSquare(enemy.x, enemy.y);
-    allEnemies.push(new Enemy);
-};
-
-var player = new Player;
+    allEnemies.push(enemy);
+}
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+
+/* Method to listen for key presses. Sends the keys to
+// Player.handleInput() method. */
 document.addEventListener('keyup', function(e) {
-        var allowedKeys = {
+    var allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
